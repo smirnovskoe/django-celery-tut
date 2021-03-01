0bc04d98d1090bb38a463506f74f85f8a54ba23c
yanix <test@gmail.com>
2021-03-01 13:49:10 +0300

    iteration 7: add Celery beat
    add periodic task for Celery

--- core/celery.py
+++ core/celery.py
@@ -1,9 +1,17 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'send-email-every-minute': {
+        'task': 'mail_sender.tasks.send_beat_email',
+        'schedule': crontab()
+    }
+}
--- mail_sender/tasks.py
+++ mail_sender/tasks.py
@@ -1,8 +1,16 @@
 from core.celery import app
-
+from .models import Contact
 from .service import test_task
 
 
 @app.task
-def send_email_by_celery(user):
+def send_email_by_celery(user_email):
+    # send(user_email)
     test_task(10, 10)
+
+
+@app.task
+def send_beat_email():
+    for contact in Contact.objects.all():
+        # send(user_email)
+        test_task(10, 10)
